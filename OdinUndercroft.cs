035a8ddcb552857a6388757dc4cb074ce609e7f0
GraveofBears <modf999@yahoo.com>
2022-02-19 22:38:01 -0500

    Add project files.

---
+++ OdinUndercroft.cs
@@ -0,0 +1,46 @@
+﻿using System.IO;
+using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+using PieceManager;
+
+//using ServerSync;
+
+namespace OdinUndercroft
+{
+    [BepInPlugin(HGUIDLower, ModName, version)]
+    public class OdinUndercroftPlugin : BaseUnityPlugin
+    {
+        public const string version = "1.0.1";
+        public const string ModName = "OdinsUndercroft";
+        internal const string Author = "Gravebear";
+        internal const string HGUID = Author + "." + "OdinsUndercroft";
+        internal const string HGUIDLower = "gravebear.odinsundercroft";
+        private const string HarmonyGUID = "Harmony." + Author + "." + ModName;
+        private static string ConfigFileName = HGUIDLower + ".cfg";
+        private static string ConfigFileFullPath = Paths.ConfigPath + Path.DirectorySeparatorChar + ConfigFileName;
+        public static string ConnectionError = "";
+
+        internal static ConfigEntry<int>? MaxNestedLimit;
+
+
+        //harmony
+        private static Harmony? harmony;
+
+        private void Awake()
+        {
+            BuildPiece buildPiece = new("odinsundercroft", "OdinsUndercroft");
+            buildPiece.Name.English("Odins Undercroft");
+            buildPiece.Description.English("A large stone basement");
+            buildPiece.RequiredItems.Add("Stone", 200, false);
+            buildPiece.Prefab.gameObject.AddComponent<Basement>();
+            harmony = new Harmony(HarmonyGUID);
+
+            harmony.PatchAll();
+            MaxNestedLimit = Config.Bind("General", "Max nested basements", 4,
+                "The maximum number of basements you can incept into each other");
+        }
+        
+        
+    }
+}
